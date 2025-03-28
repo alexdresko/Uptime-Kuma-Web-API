3d99dab9639828bcb56ac3fa02183951e428fcc2
Alex Dresko <me@alexdresko.com>
2025-03-28 13:25:10 -0400

    Add UptimeKuma docker container
    * Unfortunately, I had to include an initial database file in order to see the first user.  UK uses websockets to create the first account, and I'm not able to try to do that from postCreateCommand.sh. I'm hoping to be able to stop tracking on these files after the initial commit.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 pip3 install --user -r requirements.txt
+
+docker run -d --restart=always -p 3001:3001 -v ./data/uptime-kuma/data:/app/data --name uptime-kuma louislam/uptime-kuma:latest
